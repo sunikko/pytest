da59b6fe11738c0cdc24732029ad3eab68295d48
sunikko <sunikko77@gmail.com>
2022-06-26 01:18:45 +0100

    create mocking test : random test

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,4 +1,6 @@
 from shopping_cart import ShoppingCart
+from item_database import ItemDatabase
+from unittest.mock import Mock
 import pytest
 
 @pytest.fixture
@@ -24,9 +26,12 @@ def test_when_add_more_than_max_items_should_fail(cart):
 def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-
-    price_map = {
-        "apple": 1.0,
-        "orange": 2.0
-    }
-    assert cart.get_total_price(price_map) == 3.0
+    item_database = ItemDatabase()
+    
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        elif item == "orange":
+            return 2.0
+    item_database.get = Mock(side_effect = mock_get_item)
+    assert cart.get_total_price(item_database) == 3.0
